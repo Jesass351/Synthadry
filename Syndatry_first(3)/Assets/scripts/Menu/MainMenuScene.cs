a739f113fb843b5bda1566d07f0d239d1150b862
lixxteq <rlv1zhakov.liv@gmail.com>
2022-10-10 23:48:34 +0300

    added preloader and menu scene with example assets

---
+++ Syndatry_first(3)/Assets/scripts/Menu/MainMenuScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeSpeed = 0.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+        fadeGroup.alpha = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeSpeed;
+    }
+
+    public void OnPlayClick() {
+        Debug.Log("Changed scene: Demo");
+        SceneManager.LoadScene("Demo");
+    }
+}
