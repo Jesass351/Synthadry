bb16ef4123f48469a33c41c5c9ef2abbf81903ce
FeshLig <113169051+FeshLig@users.noreply.github.com>
2022-12-17 22:01:35 +0300

    feshlig: random loot generation

--- Syndatry_first(3)/Assets/scripts/LootController.cs
+++ Syndatry_first(3)/Assets/scripts/LootController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class LootController : MonoBehaviour
 {
     // public List<WeaponBehaviour> Weapon = new List<WeaponBehaviour>();
-    public GameObject Weapon;
+    public GameObject Katana;
+    public GameObject Shotgun;
+    public GameObject Grenade;
     public GameObject Chest;
-    private GameObject newWeapon;
+    private GameObject Weapon;
+    public int goodChance;
+    public int badChance;
     private int flag;
 
     // Update is called once per frame
@@ -16,13 +20,45 @@ public class LootController : MonoBehaviour
         flag = 1;
     }
 
+
     public void ButtonClick()
     {
-        if (flag == 1)
+        float eventChance = Random.Range(0, 99);
+        if (eventChance < goodChance)
         {
-            newWeapon = Instantiate(Weapon, Chest.transform.position, Quaternion.identity);
-            newWeapon.transform.position += transform.forward * 2 + transform.up * 2 - transform.right * 3.5f;
-            flag = 0;
+            eventChance = Random.Range(0, 99);
+            if (eventChance < 40)
+            {
+                eventChance = Random.Range(0, 2);
+                if (eventChance == 0)
+                {
+                    Weapon = Shotgun;
+                }
+                else
+                {
+                    Weapon = Katana;
+                }
+            }
+            else
+            {
+                Weapon = Grenade;
+            }
+
+            if (flag == 1)
+            {
+                Weapon = Instantiate(Weapon, Chest.transform.position, Quaternion.identity);
+                Weapon.transform.position += transform.forward * 2 + transform.up * 2 - transform.right * 3.5f;
+                flag = 0;
+            }
         }
+        else if (eventChance < goodChance + badChance)
+        {
+            Debug.Log("Отрицательное событие");
+        }
+        else
+        {
+            Debug.Log("Ничего");
+        }
+        
     }
 }
