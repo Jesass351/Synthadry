c6490092d74c2742996464667e2411feacb42061
FeshLig <113169051+FeshLig@users.noreply.github.com>
2022-12-17 21:19:41 +0300

    feshlig: genering weapon in chests

--- Syndatry_first(3)/Assets/scripts/LootController.cs
+++ Syndatry_first(3)/Assets/scripts/LootController.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class LootController : MonoBehaviour
 {
-    public List<WeaponBehaviour> Weapon = new List<WeaponBehaviour>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public List<WeaponBehaviour> Weapon = new List<WeaponBehaviour>();
+    public GameObject Weapon;
+    public GameObject Chest;
+    private GameObject newWeapon;
+    private int flag;
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        flag = 1;
+    }
+
+    public void ButtonClick()
+    {
+        if (flag == 1)
+        {
+            newWeapon = Instantiate(Weapon, Chest.transform.position, Quaternion.identity);
+            newWeapon.transform.position += transform.forward * 2 + transform.up * 2 - transform.right * 3.5f;
+            flag = 0;
+        }
     }
 }
