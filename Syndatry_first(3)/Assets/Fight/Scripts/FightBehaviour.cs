b9362642f6f2769ec2f7756c014b972feaf79562
paveldyakonov <pashadyakonov27082003@gmail.com>
2023-02-19 22:20:37 +0900

    Add WASD moving and camera rotation

---
+++ Syndatry_first(3)/Assets/Fight/Scripts/FightBehaviour.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FightBehaviour : MonoBehaviour
+{
+    
+    private CustomCharacterController _controllerManager;
+    private HealthManager _healManager;
+
+
+    [SerializeField] private GameObject player;
+    
+    [SerializeField] private GameObject cameraOld;
+    [SerializeField] private GameObject cameraNew;
+
+    [SerializeField] private GameObject mainCanvas;
+    [SerializeField] private GameObject fightCanvas;
+    [SerializeField] private GameObject deathCanvas;
+    [SerializeField] private GameObject enemyHealthBar;
+    private Animator anim;
+
+
+    public int countOfRound = 0;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && (_controllerManager.Inventory.Count > 0))
+        {
+            //��������� ���������� � ������� ��������
+            anim.SetFloat("x", 0);
+            anim.SetFloat("y", 0);
+            _controllerManager.enabled = false;
+
+            //�������� ������ ����
+            countOfRound = 1;
+
+            //������ ������ � Canvas
+            cameraOld.SetActive(false);
+            mainCanvas.SetActive(false);
+            cameraNew.SetActive(true);
+            fightCanvas.SetActive(true);
+            enemyHealthBar.SetActive(true);
+
+        }
+    }
+
+    private void Start()
+    {
+        anim = player.GetComponent<Animator>();
+        _controllerManager = player.GetComponent<CustomCharacterController>();
+    }
+
+    public void DeathEvent()
+    {
+        Destroy(player);
+
+        deathCanvas.SetActive(true);
+        fightCanvas.SetActive(false);
+        mainCanvas.SetActive(false);
+
+       
+    }
+
+    public void KillEvent()
+    {
+        cameraOld.SetActive(true);
+        cameraNew.SetActive(false);
+        mainCanvas.SetActive(true);
+        enemyHealthBar.SetActive(false);
+        _controllerManager.enabled = true;
+
+
+        Destroy(this.gameObject);
+    }
+
+    public void AdminDie()
+    {
+        DeathEvent();
+    }
+
+    public void AdminKill()
+    {
+        KillEvent();
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
