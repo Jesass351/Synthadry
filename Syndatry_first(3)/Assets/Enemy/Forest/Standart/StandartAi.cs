c709916ccb509a248f9ae83622e7f614dc0605da
FeshLig <www.artempir762003@gmail.com>
2023-05-15 02:19:25 +0300

    [feshlig] add enemy behaviour + enemy animations

--- Syndatry_first(3)/Assets/Enemy/Forest/Standart/StandartAi.cs
+++ Syndatry_first(3)/Assets/Enemy/Forest/Standart/StandartAi.cs
@@ -5,11 +5,53 @@ using UnityEngine.AI;
 
 public class StandartAi : MonoBehaviour
 {
-    public Transform target;
-    public NavMeshAgent agent;
+    private NavMeshAgent Enemy;
+    private GameObject Player;
+    private Animator anim;
+    private HPAndArmor actionTarget;
+    private float timer = 0;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        Enemy = gameObject.GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        actionTarget = Player.GetComponent<HPAndArmor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float DistationPlayer = Vector3.Distance(Player.transform.position, gameObject.transform.position);
+        if (DistationPlayer < 60) {
+            if (DistationPlayer < 8) {
+                if (DistationPlayer < 5) {
+                    Enemy.SetDestination(Enemy.transform.position);
+                    //anim.SetBool("isMove", false);
+                }
+                damage(DistationPlayer);
+            } else {
+                anim.SetBool("isAttack", false);
+                Enemy.SetDestination(Player.transform.position);
+                anim.SetBool("isMove", true);
+            }
+        } else {
+            Enemy.SetDestination(Enemy.transform.position);
+            anim.SetBool("isMove", false);
+        }
+    }
+
+    void damage(float DistationPlayer)
+    {
+        while(DistationPlayer < 8)
+        {
+            anim.SetBool("isAttack", true);
+            timer += Time.deltaTime;
+                if (timer < 1) return;
+            actionTarget.TakeDamage(10);
+            timer = 0;
+            anim.SetBool("isAttack", false);
+        }
     }
 }
